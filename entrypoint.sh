f0856c57cd9acca370fc12cc4e69a577bf13cfa8
Yurii <41921371+0x59R11@users.noreply.github.com>
2023-06-18 13:20:20 +0300

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/sh -l
 
 echo "Hello $1 ($0)"
-path=$(pwd)
-echo "Path: $path"
-lsc=$(ls -l)
-echo "$lsc"
 
-os=$(lsb_release -a)
-echo "$os"
+curenv=$(env)
+echo "$curenv"
 
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
